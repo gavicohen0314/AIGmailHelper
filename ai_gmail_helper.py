8764f4572a12bfda7af15aa4197188d2eb2b2609
Gavriel Cohen <gavrielcohen0314@gmail.com>
2024-12-04 19:32:02 +0200

    Added code to extract last 100 emails from gmail

---
+++ ai_gmail_helper.py
@@ -0,0 +1,65 @@
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import pickle
+import os.path
+
+# Define the SCOPES. If modifying it, delete the token.pickle file.
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+
+
+def getEmails():
+    creds = None  # variable to store user credentials
+
+    # Check if a user token file already exists
+    if os.path.exists('token.pickle'):
+        # Read and store user credentials
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+
+    # If there are no valid credentials, request from the user to log in
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+
+        # Save the access token in token.pickle file
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    # Connect to the Gmail API
+    service = build('gmail', 'v1', credentials=creds)
+
+    # request the last 100 emails
+    result = service.users().messages().list(maxResults=100, userId='me').execute()
+    messages = result.get('messages')  # dictionary of the last 100 emails ids
+
+    for email in messages:
+        # Get the email from its id
+        email_data = service.users().messages().get(userId='me', id=email['id']).execute()
+
+        try:
+            payload = email_data['payload']
+            headers = payload['headers']
+
+            # Extract subject (if it exists) and sender
+            for d in headers:
+                if d['name'] == 'Subject':
+                    subject = d['value']
+                if d['name'] == 'From':
+                    sender = d['value']
+
+            print("Subject: ", subject)
+            print("From: ", sender)
+        except Exception:
+            pass
+
+
+def main():
+    getEmails()
+
+
+if __name__ == "__main__":
+    main()
